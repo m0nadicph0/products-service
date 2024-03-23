37b973943e300a2572f0813b091c0a0c1860c2d0
m0nadicph0 <m0nadic.ph0@gmail.com>
2024-03-23 22:28:36 +0530

    add swagger doc for product controller

--- src/main/java/com/monadic/products/controller/ProductController.java
+++ src/main/java/com/monadic/products/controller/ProductController.java
@@ -2,10 +2,17 @@ package com.monadic.products.controller;
 
 import com.monadic.products.model.Product;
 import com.monadic.products.service.ProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 @RestController
@@ -19,24 +26,54 @@ public class ProductController {
     }
 
     @PostMapping
+    @Operation(summary = "Add a new product")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Product successfully created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Product.class))
+                    })
+    })
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         Product newProduct = productService.saveProduct(product);
         return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Get all products")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Retrieved all products",
+                    content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Product.class)))
+                    })
+    })
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a product by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the product", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content) })
     public ResponseEntity<Product> getProductById(@PathVariable String id) {
         Product product = productService.getProductById(id);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted the product", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content) })
     public ResponseEntity<?> deleteProduct(@PathVariable String id) {
         productService.deleteProduct(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
